0785d5b125d65c36537908fa1c38d2e7425242e1
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-17 20:50:51 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,17 @@
 
 def key_for_min_value(name_hash)
 
-    name_hash.each do |key, value|
-      if 
-    puts "#{key}: #{value}"
-  end
-
+    lowest_value = nil
+    lowest_name = nil
+      
+      name_hash.each do |name, value|
+        lowest_name = name if lowest_name.nil?
+        lowest_value = value if lowest_value.nil?
+        if value < lowest_value
+          lowest_value = value
+          lowest_name = name
+        end
+      end
+      
+  lowest_name
 end
